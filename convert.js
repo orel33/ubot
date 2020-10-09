1d0a3db655982f34f338d6cada58c4d55c6db015
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2020-10-09 17:24:45 +0200

    convert program for json listing v1 to v2

---
+++ convert.js
@@ -0,0 +1,59 @@
+// convert listing JSON format v1 to v2
+
+// JSON format v1 (array)
+/*
+[
+  [
+      "689504272517431308",
+      "aguermou",
+      "Abdou Guermouche",
+      "teacher"
+  ],
+  [
+      "504218107980546049",
+      "nbonicho",
+      "Nicolas Bonichon",
+      "teacher"
+  ]
+]
+*/
+
+// JSON format v2 (object)
+/*
+{
+  "689504272517431308": {
+      "discordid": "689504272517431308",
+      "userid": "aguermou",
+      "username": "Abdou Guermouche",
+      "mainrole": "teacher",
+      "extrarole": ""
+  },
+  "504218107980546049": {
+      "discordid": "504218107980546049",
+      "userid": "nbonicho",
+      "username": "Nicolas Bonichon",
+      "mainrole": "teacher",
+      "extrarole": ""
+  }
+}
+*/
+
+const fs = require("fs");
+console.log("convert listing.json => listing2.json");
+
+var dest = {};
+
+var source = JSON.parse(fs.readFileSync("listing.json", "utf8"));
+source.forEach(userinfo => {
+  dest[userinfo[0]] = {
+    'discordid': userinfo[0],
+    'userid': userinfo[1],
+    'username': userinfo[2],
+    'mainrole': userinfo[3],
+    'extrarole': ""
+  };
+});
+
+fs.writeFileSync("listing2.json", JSON.stringify(dest, null, 4), "utf8");
+
+// EOF
